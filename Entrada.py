356a7038cc61bdc035fcf32acc987dbdb5640e10
EvertonKauan <evertonkauancg@gmail.com>
2022-06-03 14:36:38 -0300

    Input de novas redes e senhas

--- Entrada.py
+++ Entrada.py
@@ -1,19 +1,17 @@
 import pymysql
-
-#Conexão criada
-conexao = pymysql.connect(host="localhost", port=3308, database="gerenciador_senhas",
-                          user="root",password="root",autocommit=True)
-
-#Cursor
+#Criar conexão
+conexao = pymysql.connect(host='localhost',port=3308,database='gerenciador_senhas',
+                          user='root',password='root',autocommit=True)
+#cursor
 cursor = conexao.cursor()
 
-#Insercao
+#input
 desejo = 'n'
 while True:
-    print("TIKTOK [1] FACEBOOK [2] INSTAGRAM [3]")
+    print("FACEBOOK [1] INSTAGRAM [2] NOVA SENHA [3]")
     c = 0
 
-    escolha = int(input("QUAL SENHA VOCÊ DESEJA ARMAZENAR? "))
+    escolha = int(input("O QUE VOCÊ DESEJA FAZER? "))
     while True:
 
         if escolha == 1 or escolha == 2 or escolha == 3:
@@ -23,23 +21,27 @@ while True:
     print()
     try:
         if escolha == 1:
-            senhatiktok = str(input("Digita ai a senha do tiktok: "))
-            comando = ("INSERT INTO senha_tiktok (senhadotiktok) values (%s)")
-            valor = senhatiktok
-            cursor.execute(comando,valor)
-            print("Senha guardada com sucesso!")
-        elif escolha == 2:
             senhaface = str(input("Digita ai a senha do facebook: "))
-            comando = ("INSERT INTO senha_facebook (senhadoface) values (%s)")
+            comando = ("INSERT INTO senha_facebook (senha_do_face) values (%s)")
             valor = senhaface
             cursor.execute(comando,valor)
             print("Senha guardada com sucesso!")
-        elif escolha == 3:
+        elif escolha == 2:
             senhainsta = str(input("Digita ai a senha do Instagram: "))
-            comando = ("INSERT INTO senha_insta (senhadoinsta) values (%s)")
+            comando = ("INSERT INTO senha_instagram (senha_do_insta) values (%s)")
             valor = senhainsta
             cursor.execute(comando, valor)
             print("Senha guardada com sucesso!")
+        elif escolha == 3:
+            descricao = str(input("Digite a rede social: "))
+            password_user = str(input("Digite a senha: "))
+            comando = ("INSERT INTO table_user (description, password) values (%s, %s)")
+            val = (descricao, password_user)
+            cursor.execute(comando,val)
+            #comando_create2 = ("INSERT INTO table_user (password) value (%s)")
+            #cursor.execute(comando_create, descricao, password_user)
+            #cursor.execute(comando_create2, password_user)
+
     except Exception as e:
         print(f"Erro: {e}")
 
@@ -48,5 +50,5 @@ while True:
 
     if desejo == "n":
         print("Programa encerrado.")
-        conexao.close()
+        conexao.close() #fechar conexão
         break
