deda03ac5a4fb660bf8432a307793d41dbab401a
EvertonKauan <evertonkauancg@gmail.com>
2022-06-03 15:14:06 -0300

    Possibilidade de visualizar as senhas já digitadas

--- Entrada.py
+++ Entrada.py
@@ -8,44 +8,48 @@ cursor = conexao.cursor()
 #input
 desejo = 'n'
 while True:
-    print("FACEBOOK [1] INSTAGRAM [2] NOVA SENHA [3]")
+    print("NOVA SENHA [1] VISUALIZAR PLATAFORMAS JÁ CADASTRADAS [2]")
     c = 0
 
     escolha = int(input("O QUE VOCÊ DESEJA FAZER? "))
     while True:
 
-        if escolha == 1 or escolha == 2 or escolha == 3:
+        if escolha == 1 or escolha == 2:
             break
 
-        escolha = int(input("Digite uma das alternativas mencionadas([1] [2] [3]): "))
+        escolha = int(input("Digite uma das alternativas mencionadas([1] [2]): "))
     print()
     try:
         if escolha == 1:
-            senhaface = str(input("Digita ai a senha do facebook: "))
-            comando = ("INSERT INTO senha_facebook (senha_do_face) values (%s)")
-            valor = senhaface
-            cursor.execute(comando,valor)
-            print("Senha guardada com sucesso!")
-        elif escolha == 2:
-            senhainsta = str(input("Digita ai a senha do Instagram: "))
-            comando = ("INSERT INTO senha_instagram (senha_do_insta) values (%s)")
-            valor = senhainsta
-            cursor.execute(comando, valor)
-            print("Senha guardada com sucesso!")
-        elif escolha == 3:
             descricao = str(input("Digite a rede social: "))
             password_user = str(input("Digite a senha: "))
             comando = ("INSERT INTO table_user (description, password) values (%s, %s)")
             val = (descricao, password_user)
             cursor.execute(comando,val)
-            #comando_create2 = ("INSERT INTO table_user (password) value (%s)")
-            #cursor.execute(comando_create, descricao, password_user)
-            #cursor.execute(comando_create2, password_user)
+        if escolha ==2:
+            consulta = ("select * from table_user")
+            cursor.execute(consulta)
+            linhas = cursor.fetchall()
+            print("Número total de plataformas cadastradas:", cursor.rowcount)
+            for linha in linhas:
+                print("Plataforma:", linha[0])
+                print("ID:", linha[2], "\n")
+
+            consulta = ("select * from table_user")
+            cursor.execute(consulta)
+            linhas = cursor.fetchall()
+            plataforma_desejada = int(input("Digite qual ID você quer visualizar: "))
+            for linha2 in linhas:
+                if plataforma_desejada == linha2[2]:
+                    print()
+                    print("Plataforma escolhida foi", linha2[0])
+                    print("Senha:", linha2[1])
+                    print()
 
     except Exception as e:
         print(f"Erro: {e}")
 
-    desejo = str(input("Deseja inserir outra senha? [s/n] "))
+    desejo = str(input("Deseja voltar ao menu? [s/n] "))
     desejo = desejo.lower()
 
     if desejo == "n":
