5b0b6d5d1abeb93cf78aa4616f29d6f07bd6a8d4
EvertonKauan <evertonkauancg@gmail.com>
2022-06-03 15:21:15 -0300

    Comentários feitos, para melhorar o entendimento

--- Entrada.py
+++ Entrada.py
@@ -5,7 +5,6 @@ conexao = pymysql.connect(host='localhost',port=3308,database='gerenciador_senha
 #cursor
 cursor = conexao.cursor()
 
-#input
 desejo = 'n'
 while True:
     print("NOVA SENHA [1] VISUALIZAR PLATAFORMAS JÁ CADASTRADAS [2]")
@@ -13,23 +12,27 @@ while True:
 
     escolha = int(input("O QUE VOCÊ DESEJA FAZER? "))
     while True:
-
         if escolha == 1 or escolha == 2:
             break
-
+        #Caso o usuário tente fugir das possibilidades
         escolha = int(input("Digite uma das alternativas mencionadas([1] [2]): "))
     print()
+
+    #Dentro de um bloco Try, para tratar os erros
     try:
+        #CRIAR NOVA SENHA evertonkauan
         if escolha == 1:
             descricao = str(input("Digite a rede social: "))
             password_user = str(input("Digite a senha: "))
-            comando = ("INSERT INTO table_user (description, password) values (%s, %s)")
-            val = (descricao, password_user)
-            cursor.execute(comando,val)
+            comando = ("INSERT INTO table_user (description, password) values (%s, %s)") #INSERIR NA TABELA A PLATAFORMA E SUA SENHA
+            val = (descricao, password_user) #criada tupla, para inserir dados na coluna
+            cursor.execute(comando,val) #execução
+
+        #VISUALIZAR SENHAS JÁ CADASTRADAS
         if escolha ==2:
-            consulta = ("select * from table_user")
+            consulta = ("select * from table_user") #selecionar a tabela, para poder visualiza-la
             cursor.execute(consulta)
-            linhas = cursor.fetchall()
+            linhas = cursor.fetchall() #contar quantas linhas existem nas colunas
             print("Número total de plataformas cadastradas:", cursor.rowcount)
             for linha in linhas:
                 print("Plataforma:", linha[0])
@@ -40,7 +43,7 @@ while True:
             linhas = cursor.fetchall()
             plataforma_desejada = int(input("Digite qual ID você quer visualizar: "))
             for linha2 in linhas:
-                if plataforma_desejada == linha2[2]:
+                if plataforma_desejada == linha2[2]: #Comparando, para quando o valor varrido for o desejado pelo usuário, exibir após o if
                     print()
                     print("Plataforma escolhida foi", linha2[0])
                     print("Senha:", linha2[1])
